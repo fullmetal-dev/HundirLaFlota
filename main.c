d5a3ade37d8084f1dc9a54178d1b77403bf85fd6
fullmetal-dev <fullmetaldev94@gmail.com>
2025-10-14 20:10:33 +0200

    feat(board): add printing on screen

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 #define MAX_WIDTH 10
 #define MAX_HEIGHT 5
+#define ASCII_A 65
 
 void init_board(int board[MAX_HEIGHT][MAX_WIDTH]) {
     for (int i = 0; i < MAX_HEIGHT; i++) {
@@ -12,7 +13,22 @@ void init_board(int board[MAX_HEIGHT][MAX_WIDTH]) {
 }
 
 void print_board(int board[MAX_HEIGHT][MAX_WIDTH]) {
+
+    printf(" "); // blank space.
+
+    // coordinates 1 - 10.
+    for (int i = 0; i < MAX_WIDTH; i++)
+        printf("%d", i);
+
+    printf("\n");
+
     for (int i = 0; i < MAX_HEIGHT; i++) {
+
+        // Coordinates A - E.
+        char letter = (int)(i + ASCII_A);
+        printf("%c", letter);
+
+        // Rest of the row.
         for (int j = 0; j < MAX_WIDTH; j++) {
             printf("%d", board[i][j]);
         }
@@ -24,7 +40,7 @@ int main(void) {
     /*
      * HUNDIR LA FLOTA
      *
-     * [ ] - Crear array bidimensional a 0 y mostrar en pantalla (junto con coordenadas).
+     * [X] - Crear array bidimensional a 0 y mostrar en pantalla (junto con coordenadas).
      * [ ] - Crear arrays barcos.
      * [ ] - Inicializar los barcos a 1 en el array inicial.
      * [ ] - Recoger respuesta de coordenadas del usuarios.
